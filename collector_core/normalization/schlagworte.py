15cc003ba87b7c66804e146e6f05f7b4a6d0456c
Julius <jt@welscheit.de>
2026-04-20 17:23:42 +0200

    Updated Contributing.md + ruff fixes

--- collector_core/normalization/schlagworte.py
+++ collector_core/normalization/schlagworte.py
@@ -82,7 +82,6 @@ def _canonicalise_ids(
     strict: bool = False,
     cutoff: float = _FUZZY_MATCH_THRESHOLD,
 ) -> list[SchlagwortIDResolution]:
-
     # C++ Matrix call
     matrix = cdist(
         raw_ids,
