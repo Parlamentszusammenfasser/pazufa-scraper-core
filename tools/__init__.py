3eac6639bc2f41e33d73d11b77e96d457b79d19d
ILoveCode <juliustim@proton.me>
2026-05-14 15:21:33 +0200

    Added __init__ file for tool folder, incuded the yaml file validation tools.

---
+++ tools/__init__.py
@@ -0,0 +1,13 @@
+"""
+Tools for pazufa_corelib
+"""
+
+from yaml_validator_authors import yaml_validator_authors
+from yaml_validator_organizations import yaml_validator_organizations
+from yaml_validator_tags import yaml_validator_tags
+
+__all__ = [
+    "yaml_validator_authors",
+    "yaml_validator_organizations",
+    "yaml_validator_tags",
+]
