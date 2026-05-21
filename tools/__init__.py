e6e148cc61f01b85033a368385f05a402bb0abd8
flovar <flovar@noreply.codeberg.org>
2026-05-21 08:16:37 +0100

    fix: empty to contain only docstring so imports work

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,13 +1,1 @@
-"""
-Tools for pazufa_corelib
-"""
-
-from yaml_validator_authors import yaml_validator_authors
-from yaml_validator_organizations import yaml_validator_organizations
-from yaml_validator_tags import yaml_validator_tags
-
-__all__ = [
-    "yaml_validator_authors",
-    "yaml_validator_organizations",
-    "yaml_validator_tags",
-]
+"""Tools for pazufa_corelib."""
